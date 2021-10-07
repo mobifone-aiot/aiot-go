cef44d74f30ac58fc2c4615681c4d3300ee8ec90
thanhdaon <thanhdaon89@gmail.com>
2021-10-07 09:18:41 +0700

    module changes for v0.0.1

--- client_examples_test.go
+++ client_examples_test.go
@@ -8,7 +8,7 @@ import (
 )
 
 func ExampleClient() {
-	// Tạo một aiot client và thực hiện lệnh laasy token cho một user
+	// Tạo một aiot client và thực hiện lệnh lấy token cho một user
 
 	client := aiot.New("http://localhost")
 
@@ -18,4 +18,28 @@ func ExampleClient() {
 	}
 
 	fmt.Printf("Token: %s", token)
+}
+
+func ExampleClient_CreateThings() {
+	// Tạo một aiot client và thực hiện lệnh tạo things
+
+	client := aiot.New("http://localhost")
+
+	token, err := client.Token("email@demo.com", "password")
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	thingNames := []string{
+		"demo-1",
+		"demo-2",
+		"demo-3",
+	}
+
+	things, err := client.CreateThings(token, thingNames)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	fmt.Printf("Things: %v", things)
 }
